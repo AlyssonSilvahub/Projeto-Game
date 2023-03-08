8888bda423ea568ddeae8e345118a9e6f7c341b5
Alysson Silva <90972564+AlyssonSilvahub@users.noreply.github.com>
2023-03-08 02:53:56 -0300

    Div

--- index.js
+++ index.js
@@ -158,9 +158,8 @@ if (keys.ArrowLeft.pressed && enemy.lastkey === "ArrowLeft") {
     player.isAttacking
   ) {
     player.isAttacking = false;
-    console.log("go");
     enemy.health = -20;
-    document.querySelector("#enemyHealth").style.width = enemy.health + "%";
+    document.querySelector("#enemyHealth").style.width = enemy.health + "%20";
   }
   if (
     rectangularCollision({

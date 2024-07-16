34495e28da8a13450a7b810e04905f3fc24b9009
Alysson Silva <90972564+AlyssonSilvahub@users.noreply.github.com>
2024-07-16 02:56:40 -0300

    "Added attackBox properties to Fighter class and instances, updated collision detection and attack logic in animate function"

--- js/classes.js
+++ js/classes.js
@@ -59,6 +59,7 @@ class Fighter extends Sprite {
     framesMax = 1,
     offset = { x: 0, y: 0 },
     sprites,
+    attackBox = { offset: {}, width: undefined, height: undefined },
   }) {
     super({
       position,
@@ -77,9 +78,9 @@ class Fighter extends Sprite {
         x: this.position.x,
         y: this.position.y,
       },
-      offset,
-      width: 100,
-      height: 50,
+      offset: attackBox.offset,
+      width: attackBox.width,
+      height: attackBox.height,
     };
     this.color = color;
     this.isAttacking;
@@ -100,7 +101,15 @@ class Fighter extends Sprite {
     this.animateFrames();
 
     this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
-    this.attackBox.position.y = this.position.y;
+    this.attackBox.position.y = this.position.y + this.attackBox.offset.y;
+
+    //attack box
+    /*c.fillRect(
+      this.attackBox.position.x,
+      this.attackBox.position.y,
+      this.attackBox.width,
+      this.attackBox.height
+    );*/
 
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
@@ -113,9 +122,6 @@ class Fighter extends Sprite {
   attack() {
     this.switchSprite("attack1");
     this.isAttacking = true;
-    setTimeout(() => {
-      this.isAttacking = false;
-    }, 100);
   }
   switchSprite(sprite) {
     if (
